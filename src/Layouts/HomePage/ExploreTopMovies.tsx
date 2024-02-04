074eef806228ff618e18b4637708adcacd9d1084
Cameron Earl <cearl59@wgu.edu>
2024-02-03 23:26:19 -0500

    Adds layouts folder to hold different pages made. Makes HomePage and ExploreTopMovies movies page

---
+++ src/Layouts/HomePage/ExploreTopMovies.tsx
@@ -0,0 +1,16 @@
+export const ExportTopBooks = () => {
+    return (
+        <div className='p-5 mb-4 bg-dark header'>
+            <div className='container-fluid py-5 text-white 
+            d-flex justify-content-center align-items-center'>
+                <div>
+                    <h1 className='display-5 fw-bold'>Check Out These Picks</h1>
+                    <p className='col-md-8 fs-4'>Check Out More</p>
+                    <a type='button' className='btn main-color btn-lg text-white' href='#'>
+                        Explore Top Picks
+                    </a>
+                </div>
+            </div>
+        </div>
+    )
+}
